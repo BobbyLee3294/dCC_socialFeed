2abcf6c1df2b832a440fbb392136b9ae8e049e69
Bobby Lee <thayungbobby@gmail.com>
2022-12-18 16:15:52 -0500

    added minor bootstrap styling

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Post from "./Components/Post/Post";
+import PostList from "./Components/PostList/PostList";
+import CreatePostForm from "./Components/CreatePostForm/CreatePostForm";
+import NavBar from "./Components/NavBar/NavBar";
+// import './Components/App.css';
 
 function App() {
+  const [post, setPost] = useState([
+    {
+      name: "Jimbo Jones",
+      post: "This is my first post! Give me a like, please!",
+    },
+  ]);
+
+  function addNewPost(entry) {
+    let tempPosts = [entry, ...post];
+    setPost(tempPosts);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container-fluid">
+        <div id="NavBar">
+          <NavBar />
+        </div>
+      </div>
+      <div className="container-md">
+        <div className="row">
+          <CreatePostForm addNewPost={addNewPost} />
+        </div>
+        <div className="row">
+          <Post />
+          <PostList />
+        </div>
+      </div>
     </div>
   );
 }
