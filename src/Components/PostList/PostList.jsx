07903d978980e4cc554c182fbb8b4c247e538f89
Bobby Lee <thayungbobby@gmail.com>
2022-12-18 16:26:59 -0500

    created TODO# 2

---
+++ src/Components/PostList/PostList.jsx
@@ -0,0 +1,9 @@
+const PostList = (props) => {
+  return (
+    //**TODO #2: Create the feed where all the posts will be displayed.
+    // Must be passed into using props. Utilize the map higher order array method to map each post to its own Post component */
+    <div></div>
+  );
+};
+
+export default PostList;
