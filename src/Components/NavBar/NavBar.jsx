d84e575cd0fc944a3350728c60767a106f7a1593
Bobby Lee <thayungbobby@gmail.com>
2022-12-18 16:37:39 -0500

    created TODO FINAL

---
+++ src/Components/NavBar/NavBar.jsx
@@ -0,0 +1,12 @@
+// import './Components/App.css';
+
+const NavBar = (props) => {
+  return (
+    //**TODO #FINAL: Create a NavBar that sticks to the top of page */
+    <div class="topnav">
+      <a href="#home">Home</a>
+    </div>
+  );
+};
+
+export default NavBar;
