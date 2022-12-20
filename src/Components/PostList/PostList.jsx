71d69a81da181927ad884c4e6753354d336c95d4
Bobby Lee <thayungbobby@gmail.com>
2022-12-19 21:04:18 -0500

    completed TODO #2 (SO to Amy for the tips)

--- src/Components/PostList/PostList.jsx
+++ src/Components/PostList/PostList.jsx
@@ -1,8 +1,15 @@
-const PostList = (props) => {
+import Post from "../Post/Post";
+const PostList = ({parentPosts}) => {
   return (
     //**TODO #2: Create the feed where all the posts will be displayed.
     // Must be passed into using props. Utilize the map higher order array method to map each post to its own Post component */
-    <div></div>
+    <div>
+      {parentPosts.map(
+        (post, index) => (
+          <Post key={index} post={post}/>
+        )
+      )}
+    </div>
   );
 };
 
