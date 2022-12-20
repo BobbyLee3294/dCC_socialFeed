74e9a0eac9cb010e33079c6017bb0c1077b63aac
Bobby Lee <thayungbobby@gmail.com>
2022-12-20 01:10:57 -0500

    corrected naming from user to form & removed id attr

--- src/Components/CreatePostForm/CreatePostForm.jsx
+++ src/Components/CreatePostForm/CreatePostForm.jsx
@@ -4,15 +4,15 @@ import "./CreatePostForm.css";
 const CreatePostForm = (props) => {
   //**TODO #3: Create a form with 2 input fields. One for the user's name and one for the body of their post
   //Each should be bound to its own state variable */
-  const [userName, setUserName] = useState("");
-  const [postBody, setPostBody] = useState("");
+  const [formName, setFormName] = useState("");
+  const [formBody, setFormBody] = useState("");
   //**TODO #3a: Implement a handleSubmit method for your form. That function should create a new post object,
   // add it the existing array of posts and update the App.js state variable with the new array.*/
   function handleSubmit(event) {
     event.preventDefault();
     let newPost = {
-      name: userName,
-      body: postBody,
+      name: formName,
+      body: formBody,
     };
     // console.log(newPost);
     props.addNewPost(newPost);
@@ -22,29 +22,29 @@ const CreatePostForm = (props) => {
     <div className="container">
       <form id="post-form" onSubmit={handleSubmit}>
         <div className="mb-1 row">
-          <label htmlFor="userName" className="col-sm-1" id="labelName">
+          <label htmlFor="formName" className="col-sm-1">
             Name
           </label>
           <div className="col-sm-11">
             <input
               type="text"
-              id="postUserName"
-              value={userName}
-              onChange={(event) => setUserName(event.target.value)}
+              id="formName"
+              value={formName}
+              onChange={(event) => setFormName(event.target.value)}
             />
           </div>
         </div>
         <div className="mb-auto row">
-          <label htmlFor="postBody" className="col-sm-1" id="labelBody">
+          <label htmlFor="formBody" className="col-sm-1">
             Post
           </label>
           <div className="col-sm-11">
             <textarea
-              id="postBody"
+              id="formBody"
               placeholder="Write something.."
-              value={postBody}
+              value={formBody}
               maxLength="120"
-              onChange={(event) => setPostBody(event.target.value)}
+              onChange={(event) => setFormBody(event.target.value)}
             />
           </div>
         </div>
