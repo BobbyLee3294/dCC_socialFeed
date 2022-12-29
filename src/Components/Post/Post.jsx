433be84193d27996ce5f916a0be4858c54499a17
Bobby Lee <thayungbobby@gmail.com>
2022-12-29 18:56:42 -0500

    completed TODO #4a & 4b

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import { FaThumbsUp } from "react-icons/fa";
 import { FaThumbsDown } from "react-icons/fa";
 import "./Post.css";
@@ -7,21 +8,46 @@ const Post = (props) => {
   //create a function for each button when they are clicked then bind them to an onClick event*/
   //**TODO #4a: Figure out why first post buttons toggle */
   //**TODO #4b: Figure out how to have ONLY one button toggle at a time(ie: if like toggled on, then dislike is toggled off and vice versa) */
-  function thumbsUpToggle() {
-    document.getElementById("thumbsUp").classList.toggle("likeColored");
+
+  const [likeactive, setLikeactive] = useState(false);
+  const [dislikeactive, setDislikeactive] = useState(false);
+
+  function likeFunction() {
+    if (likeactive) {
+      setLikeactive(false);
+    } else {
+      setLikeactive(true);
+      if (dislikeactive) {
+        setDislikeactive(false);
+      }
+    }
   }
-  function thumbsDownToggle() {
-    document.getElementById("thumbsDown").classList.toggle("dislikeColored");
+
+  function dislikeFunction() {
+    if (dislikeactive) {
+      setDislikeactive(false);
+    } else {
+      setDislikeactive(true);
+      if (likeactive) {
+        setLikeactive(false);
+      }
+    }
   }
   return (
     //**TODO #1: Create a template which will include the user's name, the body of the post and the "like" and "dislike" buttons */
     <div id="postLayout">
       <p id="postName">{props.post.name}</p>
       <p id="postBody">{props.post.body}</p>
-      <button id="thumbsUp" onClick={thumbsUpToggle}>
+      <button
+        onClick={likeFunction}
+        className={[likeactive ? "likeactive" : null, "button"].join(" ")}
+      >
         <FaThumbsUp />
       </button>
-      <button id="thumbsDown" onClick={thumbsDownToggle}>
+      <button
+        onClick={dislikeFunction}
+        className={[dislikeactive ? "dislikeactive" : null, "button"].join(" ")}
+      >
         <FaThumbsDown />
       </button>
     </div>
