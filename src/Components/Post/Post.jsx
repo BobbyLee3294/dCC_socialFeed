10c4c2cf2742a4a37b5b612f44261f14986b7c4e
Bobby Lee <thayungbobby@gmail.com>
2022-12-29 19:36:44 -0500

    added BONUS TODO

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -8,7 +8,8 @@ const Post = (props) => {
   //create a function for each button when they are clicked then bind them to an onClick event*/
   //**TODO #4a: Figure out why first post buttons toggle */
   //**TODO #4b: Figure out how to have ONLY one button toggle at a time(ie: if like toggled on, then dislike is toggled off and vice versa) */
-
+  //*BONUS TODO: add dates to all posts and post creations
+  
   const [likeactive, setLikeactive] = useState(false);
   const [dislikeactive, setDislikeactive] = useState(false);
 
