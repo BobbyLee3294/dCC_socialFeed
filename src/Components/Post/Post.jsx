61934777b7bfa354c3e8c93135529f2f862d2c86
Bobby Lee <thayungbobby@gmail.com>
2022-12-29 20:05:15 -0500

    added BONUS TODO

--- src/Components/Post/Post.jsx
+++ src/Components/Post/Post.jsx
@@ -1,6 +1,5 @@
-import React, { useState } from "react";
-import { FaThumbsUp } from "react-icons/fa";
-import { FaThumbsDown } from "react-icons/fa";
+import React from "react";
+import LikeButton from "../LikeButton/LikeButton";
 import "./Post.css";
 
 const Post = (props) => {
@@ -8,37 +7,15 @@ const Post = (props) => {
   //create a function for each button when they are clicked then bind them to an onClick event*/
   //**TODO #4a: Figure out why first post buttons toggle */
   //**TODO #4b: Figure out how to have ONLY one button toggle at a time(ie: if like toggled on, then dislike is toggled off and vice versa) */
+  //*BONUS TODO: add dates to all posts and post creations
 
-  const [likeactive, setLikeactive] = useState(false);
-  const [dislikeactive, setDislikeactive] = useState(false);
-
-  function likeFunction() {
-    if (likeactive) {
-      setLikeactive(false);
-    } else {
-      setLikeactive(true);
-      if (dislikeactive) {
-        setDislikeactive(false);
-      }
-    }
-  }
-
-  function dislikeFunction() {
-    if (dislikeactive) {
-      setDislikeactive(false);
-    } else {
-      setDislikeactive(true);
-      if (likeactive) {
-        setLikeactive(false);
-      }
-    }
-  }
-  return (
+    return (
     //**TODO #1: Create a template which will include the user's name, the body of the post and the "like" and "dislike" buttons */
     <div id="postLayout">
       <p id="postName">{props.post.name}</p>
+      <span className="xyz">{`${new Date().toLocaleString()}`}</span>
       <p id="postBody">{props.post.body}</p>
-      <button
+      {/* <button
         onClick={likeFunction}
         className={[likeactive ? "likeactive" : null, "button"].join(" ")}
       >
@@ -49,7 +26,8 @@ const Post = (props) => {
         className={[dislikeactive ? "dislikeactive" : null, "button"].join(" ")}
       >
         <FaThumbsDown />
-      </button>
+      </button> */}
+      <LikeButton />
     </div>
   );
 };
