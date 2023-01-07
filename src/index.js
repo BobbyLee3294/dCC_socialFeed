bedc7a3e3076e68199f3e910ee2a825bbd32d8e6
Bobby Lee <thayungbobby@gmail.com>
2023-01-07 12:52:45 -0500

    removed comments

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
+//**using methods for react v18 */
 import {createRoot} from 'react-dom/client';
 import App from './App';
 import 'bootstrap/dist/css/bootstrap.min.css';
-
-// ReactDOM.render(<App />, document.getElementById(`root`));
 
 const container = document.getElementById('root');
 const root = createRoot(container);
